c0f5152991149b377793b0d9d1ecae2675f53478
firegoodwarrior <48214390+firegoodwarrior@users.noreply.github.com>
2019-05-28 23:28:26 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,21 +1,34 @@
 import urllib.request
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
+from selenium import webdriver
 
-f = open("C:/test/channel.txt", 'r')#channel url을 모은 text파일
+f = open("test/channel.txt", 'r')#channel url을 모은 text파일
 while True:
     line = f.readline()#한 줄씩 (=한채널씩)읽어와서
     if not line : break
-    print(line)
     req = urllib.request
     d = req.urlopen(line)
     stat = d.getheaders()
-    if d.status is 200:#채널이 활성화 상태면 
+    if d.status is 200:#채널이 활성화 상태면
         print("Connect Success")
         print("Get Comment Information")
         req = requests.get(line)
-        html = req.text
-        soup = BeautifulSoup(html, 'html.parser')
+        #print(line)
+        html = req.content.decode('utf-8', 'replace')
+        soup = BeautifulSoup(html, "html.parser")
+        k = soup.find_all("h1", "branded-page-header-title")
+        for i in k:
+            name = i.text.strip().replace('\n', ' ').replace(',', '')
+            print(name)
+        st = "/videos"
+        search_line = line.rstrip('\n')#개행문자 제거
+        search_line += st
+        print(search_line)
+        driver = webdriver.Chrome()
+        driver.get(search_line)
+
+
     else:
         print("Fail")
         print("Get Next Channel")
