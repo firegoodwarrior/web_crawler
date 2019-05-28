fbe7782fe3be4e6c09d7932280eb1d1665b3a0e4
firegoodwarrior <48214390+firegoodwarrior@users.noreply.github.com>
2019-05-29 00:35:57 +0900

    Update crawler.py
    유튜버 id 받아오기 기능 추가
    웹 문서 저장 추가
    (추후에 내부에서 영상 url찾아 detect할 예정)

--- crawler.py
+++ crawler.py
@@ -2,8 +2,9 @@ import urllib.request
 import requests
 from bs4 import BeautifulSoup, Tag
 from selenium import webdriver
-
+import pytube
 f = open("test/channel.txt", 'r')#channel url을 모은 text파일
+number=0
 while True:
     line = f.readline()#한 줄씩 (=한채널씩)읽어와서
     if not line : break
@@ -18,6 +19,7 @@ while True:
         html = req.content.decode('utf-8', 'replace')
         soup = BeautifulSoup(html, "html.parser")
         k = soup.find_all("h1", "branded-page-header-title")
+        name=""
         for i in k:
             name = i.text.strip().replace('\n', ' ').replace(',', '')
             print(name)
@@ -27,12 +29,17 @@ while True:
         print(search_line)
         driver = webdriver.Chrome()
         driver.get(search_line)
-
-
+        req2 = requests.get(search_line)
+        html2 = req2.content.decode('utf-8', 'replace')#html을 utf-8로 디코드 작업
+        soup2 = BeautifulSoup(html2, "lxml")#파싱 시작
+        output = "test/list"+str(number)+".txt"
+        file = open(output,'w',encoding='utf8')
+        file.write(str(soup2))
+        file.close()
     else:
         print("Fail")
         print("Get Next Channel")
-
+    number+=1
 
 
 f.close()
