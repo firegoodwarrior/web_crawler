da7f29d7aff77820c6682fde77970d7422df50b7
firegoodwarrior <48214390+firegoodwarrior@users.noreply.github.com>
2019-05-29 19:47:21 +0900

    Update crawler.py
    채널에서 동영상 탐색(현재30개) -> 동영상 하나하나의 링크로 들어가서 html구조 파악까지 완료.
    
    다만 유튜브 특성상 스크롤을 쭉 내려야 댓글과 다른 추천 영상 표기됨
    
    즉
    
    1. 영상 진입 후 스크롤을 쭉 내리고
    2. 그 후에 html 구조 파악
    3. 그 담엔 내가 comments쪽 알아내서 데이터 수집하기
    
    곧 데이터 수집을 시작할 수 있을 것 같다.

--- crawler.py
+++ crawler.py
@@ -7,19 +7,20 @@ f = open("test/channel.txt", 'r')#channel url을 모은 text파일
 number=0
 while True:
     line = f.readline()#한 줄씩 (=한채널씩)읽어와서
-    if not line : break
+    if not line:
+        break
     req = urllib.request
     d = req.urlopen(line)
     stat = d.getheaders()
     if d.status is 200:#채널이 활성화 상태면
         print("Connect Success")
         print("Get Comment Information")
-        req = requests.get(line)
+        req = requests.get(line)#채널 진입
         #print(line)
         html = req.content.decode('utf-8', 'replace')
         soup = BeautifulSoup(html, "html.parser")
         k = soup.find_all("h1", "branded-page-header-title")
-        name=""
+        name = ""
         for i in k:
             name = i.text.strip().replace('\n', ' ').replace(',', '')
             print(name)
@@ -30,12 +31,28 @@ while True:
         driver = webdriver.Chrome()
         driver.get(search_line)
         req2 = requests.get(search_line)
-        html2 = req2.content.decode('utf-8', 'replace')#html을 utf-8로 디코드 작업
-        soup2 = BeautifulSoup(html2, "lxml")#파싱 시작
-        output = "test/list"+str(number)+".txt"
-        file = open(output,'w',encoding='utf8')
-        file.write(str(soup2))
-        file.close()
+        html2 = req2.content.decode('utf8', 'replace')
+        soup2 = BeautifulSoup(html2, "html.parser")
+        finders = soup2.findAll("h3", {"class": "yt-lockup-title"})
+        num = 1
+        for finder in finders:
+            finder = str(finder)
+            #print(finder)
+            addr = finder.partition('href="/')[2]
+            addr = addr.split('"')[0]
+          #  print(addr)
+            goal = "https://www.youtube.com/"+addr
+            driver.get(goal)
+            req3 = requests.get(goal)
+            html3 = req3.content.decode('utf-8','replace')
+            soup3 = BeautifulSoup(html3, "lxml")
+            output = 'test/'+str(number)+"_"+str(num)+'.txt'
+            file = open(output, 'w',encoding='utf8') # 내부 구조를 보기 위한것. encoding 작업을 하지 않으면 오류나니까 유의
+            file.write(str(soup3))
+            file.close()
+            num += 1
+
+        driver.close()
     else:
         print("Fail")
         print("Get Next Channel")
