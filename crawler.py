715839daeef8c6af930a5303d27e35c449c7233a
firegoodwarrior <48214390+firegoodwarrior@users.noreply.github.com>
2019-05-30 11:27:48 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -2,6 +2,8 @@ import urllib.request
 import requests
 from bs4 import BeautifulSoup, Tag
 from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
 import pytube
 f = open("test/channel.txt", 'r')#channel url을 모은 text파일
 number=0
@@ -43,11 +45,20 @@ while True:
           #  print(addr)
             goal = "https://www.youtube.com/"+addr
             driver.get(goal)
-            req3 = requests.get(goal)
-            html3 = req3.content.decode('utf-8','replace')
-            soup3 = BeautifulSoup(html3, "lxml")
+            page_down_cnt = 50
+            start_scroll=0
+            while page_down_cnt:
+                #driver.execute_script("window.scrollTo(start_scroll,document.body.scrollHeight);")
+                driver.find_element_by_tag_name('body').send_keys(Keys.END)
+                page_down_cnt -= 1 # send_keys(Keys.END)로 아래까지 쭉 search
+                time.sleep(0.3) 
+            #req3 = requests.get(goal)
+            #html3 = req3.content.decode('utf-8','replace')
+            #soup3 = BeautifulSoup(html3, "lxml")
+            html3=driver.page_source #html3 를 현재 page_source로 다 저장.
+            soup3 = BeautifulSoup(html3, "html.parser")
             output = 'test/'+str(number)+"_"+str(num)+'.txt'
-            file = open(output, 'w',encoding='utf8') # 내부 구조를 보기 위한것. encoding 작업을 하지 않으면 오류나니까 유의
+            file = open(output, 'w', encoding='utf8') # 내부 구조를 보기 위한것. encoding 작업을 하지 않으면 오류나니까 유의
             file.write(str(soup3))
             file.close()
             num += 1
